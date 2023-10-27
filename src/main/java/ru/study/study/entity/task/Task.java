7c3a1afbc016387319581c92a1bc92676cbb96cd
SergioPetrushin <buldozer92@list.ru>
2023-10-27 23:02:55 +0300

    Создана сущность Task

---
+++ src/main/java/ru/study/study/entity/task/Task.java
@@ -0,0 +1,55 @@
+package ru.study.study.entity.task;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ru.study.study.entity.EntityWithName;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "TASK")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true, callSuper = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@AttributeOverride(name = "id", column = @Column(name = "TASK_ID"))
+public class Task extends EntityWithName {
+    public Task(long id) {
+
+        this.id = id;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PLAN_ID")
+    private EducationPlan plan;
+
+    @Override
+    public Task setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public Task setCreated(LocalDateTime created) {
+        this.created = created;
+        return this;
+    }
+
+    @Override
+    public Task setModified(LocalDateTime modified) {
+        this.modified = modified;
+        return this;
+    }
+
+    @Override
+    public Task setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public Task setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+}
