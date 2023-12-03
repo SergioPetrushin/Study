466174207a2a148987812f875ebb04e38eb91057
SergioPetrushin <buldozer92@list.ru>
2023-12-03 21:17:35 +0300

    Реализованы доменные и сервисные сервисы FileDz без использования Student Task(Так как он в другой ветке) нужно исправить

---
+++ src/main/java/ru/study/study/dto/request/filedz/FileDzRequest.java
@@ -0,0 +1,15 @@
+package ru.study.study.dto.request.filedz;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@ToString
+@EqualsAndHashCode
+
+public class FileDzRequest {
+    private Long fileId;
+}
