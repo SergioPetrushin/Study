3158562963d5ea44f859feda70747c38996d91be
SergioPetrushin <buldozer92@list.ru>
2023-12-05 23:24:43 +0300

    Исправлен UserMerger, добавлены @Service                          @RequiredArgsConstructor

--- src/main/java/ru/study/study/mapper/user/UserMerger.java
+++ src/main/java/ru/study/study/mapper/user/UserMerger.java
@@ -1,9 +1,12 @@
 package ru.study.study.mapper.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.study.study.dto.request.user.UserAddRequest;
 import ru.study.study.entity.user.User;
 import ru.study.study.mapper.Merger;
-
+@Service
+@RequiredArgsConstructor
 public class UserMerger implements Merger<User, UserAddRequest> {
     @Override
     public User merge(User target, UserAddRequest source) {
