064666af2473f560830715303900d0b8e977505b
vyushin_aa <ledford1403@gmail.com>
2024-01-23 20:06:02 +0300

    Cleanup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,83 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Login from "@/pages/Login.vue";
 import Tasks from "@/pages/Tasks.vue";
 import EducationPlans from "@/pages/EducationPlans.vue";
-import TaskStatus from "@/pages/TaskStatus.vue";
-import UserTypes from "@/pages/UserTypes.vue";
 import Gradebook from "@/pages/Gradebook.vue";
 import Messages from "@/pages/Messages.vue";
-import UserStatus from "@/pages/UserStatus.vue";
-import Students from "@/pages/Students.vue";
-import Teachers from "@/pages/Teachers.vue";
 import AllUsers from "@/pages/AllUsers.vue";
+import Registration from "@/pages/Registration.vue";
 
 
 const routes = [
 
-  {
-    path : '/login',
-    component : Login
-  },
+    {
+        path: '/login',
+        component: Login
+    },
 
-  {
-    path : '/tasks',
-    component : Tasks
-  },
-  {
-    path : '/education-plans',
-    component : EducationPlans
-  },
+    {
+        path: '/registration',
+        component: Registration
+    },
 
-  {
-    path : '/admin/task-status',
-    component : TaskStatus
-  },
+    {
+        path: '/tasks',
+        component: Tasks
+    },
+    {
+        path: '/education-plans',
+        component: EducationPlans
+    },
 
-  {
-    path : '/admin/user-type',
-    component : UserTypes
-  },
+    {
+        path: '/gradebook',
+        component: Gradebook
+    },
 
-  {
-    path : '/gradebook',
-    component : Gradebook
-  },
-
-  {
-    path : '/messages',
-    component : Messages
-  },
-
-  {
-    path : '/admin/user-status',
-    component : UserStatus
-  },
-
-  {
-    path : '/admin/students',
-    component : Students
-  },
-
-  {
-    path : '/admin/teachers',
-    component : Teachers
-  },
-
-  {
-    path : '/admin/all-users',
-    component : AllUsers
-  },
+    {
+        path: '/messages',
+        component: Messages
+    },
 
 
-
-
+    {
+        path: '/admin/users',
+        component: AllUsers
+    },
 
 
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
